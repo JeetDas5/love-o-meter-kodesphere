8f98966be11900a367074f671f9a1ce4305fb698
JeetDas5 <jeet15083011@gmail.com>
2025-04-07 15:17:15 +0530

    deployed backend

--- src/components/SendLoveData.tsx
+++ src/components/SendLoveData.tsx
@@ -1,6 +1,6 @@
 const sendLoveData = async (name: string, crush: string, result: number) => {
   try {
-    const response = await fetch(`${import.meta.env.VITE_PORT}/api/love`, {
+    const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/love`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
